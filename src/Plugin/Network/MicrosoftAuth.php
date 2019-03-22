98638e4ba64799ea81f2bd540b5d6b0d90bf2927
Getulio Sánchez <valentin2507@gmail.com>
2019-03-22 13:37:30 -0400

    Issue #3042452: Use Social Auth Network Base

--- src/Plugin/Network/MicrosoftAuth.php
+++ src/Plugin/Network/MicrosoftAuth.php
@@ -2,16 +2,11 @@
 
 namespace Drupal\social_auth_microsoft\Plugin\Network;
 
-use Drupal\Core\Config\ConfigFactoryInterface;
-use Drupal\Core\Entity\EntityTypeManagerInterface;
-use Drupal\Core\Logger\LoggerChannelFactoryInterface;
 use Drupal\Core\Url;
-use Drupal\social_api\Plugin\NetworkBase;
 use Drupal\social_api\SocialApiException;
+use Drupal\social_auth\Plugin\Network\NetworkBase;
 use Drupal\social_auth_microsoft\Settings\MicrosoftAuthSettings;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 use Stevenmaguire\OAuth2\Client\Provider\Microsoft;
-use Drupal\Core\Site\Settings;
 
 /**
  * Defines a Network Plugin for Social Auth Microsoft.
@@ -33,67 +28,6 @@ use Drupal\Core\Site\Settings;
 class MicrosoftAuth extends NetworkBase implements MicrosoftAuthInterface {
 
   /**
-   * The logger factory.
-   *
-   * @var \Drupal\Core\Logger\LoggerChannelFactory
-   */
-  protected $loggerFactory;
-
-  /**
-   * The site settings.
-   *
-   * @var \Drupal\Core\Site\Settings
-   */
-  protected $siteSettings;
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    return new static(
-      $configuration,
-      $plugin_id,
-      $plugin_definition,
-      $container->get('entity_type.manager'),
-      $container->get('config.factory'),
-      $container->get('logger.factory'),
-      $container->get('settings')
-    );
-  }
-
-  /**
-   * MicrosoftAuth constructor.
-   *
-   * @param array $configuration
-   *   A configuration array containing information about the plugin instance.
-   * @param string $plugin_id
-   *   The plugin_id for the plugin instance.
-   * @param array $plugin_definition
-   *   The plugin implementation definition.
-   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
-   *   The entity type manager.
-   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
-   *   The configuration factory object.
-   * @param \Drupal\Core\Logger\LoggerChannelFactoryInterface $logger_factory
-   *   The logger factory.
-   * @param \Drupal\Core\Site\Settings $settings
-   *   The site settings.
-   */
-  public function __construct(array $configuration,
-                              $plugin_id,
-                              array $plugin_definition,
-                              EntityTypeManagerInterface $entity_type_manager,
-                              ConfigFactoryInterface $config_factory,
-                              LoggerChannelFactoryInterface $logger_factory,
-                              Settings $settings) {
-
-    parent::__construct($configuration, $plugin_id, $plugin_definition, $entity_type_manager, $config_factory);
-
-    $this->loggerFactory = $logger_factory;
-    $this->siteSettings = $settings;
-  }
-
-  /**
    * Sets the underlying SDK library.
    *
    * @return \Stevenmaguire\OAuth2\Client\Provider\Microsoft|false
@@ -106,7 +40,7 @@ class MicrosoftAuth extends NetworkBase implements MicrosoftAuthInterface {
 
     $class_name = 'Stevenmaguire\OAuth2\Client\Provider\Microsoft';
     if (!class_exists($class_name)) {
-      throw new SocialApiException(sprintf('The Microsoft Library for the league oAuth not found. Class: %s.', $class_name));
+      throw new SocialApiException(sprintf('The Microsoft library for PHP League OAuth2 not found. Class: %s.', $class_name));
     }
 
     /* @var \Drupal\social_auth_microsoft\Settings\MicrosoftAuthSettings $settings */
@@ -149,6 +83,7 @@ class MicrosoftAuth extends NetworkBase implements MicrosoftAuthInterface {
       $this->loggerFactory
         ->get('social_auth_microsoft')
         ->error('Define App ID and App Secret on module settings.');
+
       return FALSE;
     }
 
